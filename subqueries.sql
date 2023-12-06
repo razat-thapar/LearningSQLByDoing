a347f91f528e796c137d34bffabfec22c67963b5
RazatAggarwal <razat.aggarwal@outlook.com>
2023-12-06 13:52:01 +0530

    added few queries

--- subqueries.sql
+++ subqueries.sql
@@ -53,15 +53,45 @@ WHERE p.productLine='Motorcycles'
 		-- 2. SELECT Those film id's whose actors IN result set of 1. 	
 		SELECT fa.film_id
 		FROM sakila.film_actor fa
-		WHERE fa.actor_id IN 
+		WHERE fa.actor_id IN
 		(
 			SELECT a.actor_id
 			FROM sakila.actor a 
 			WHERE a.last_name ='NEESON'
 		);
--- ALL operator
-	-- give me all films 
--- Exists operator
+-- ANY operator. (Matches any one value in the list)
+-- CASE 1: equating with a list
+-- WHERE bid =ANY(1,2,3)  is equivalent to WHERE bid IN (1,2,3); 
+-- Case 2: non-equal compare with a list 
+-- WHERE bid >ANY(1,2,3)   not possible to do using IN() 
+	-- give me all students having psp > any one psp in the list. 
+	SELECT * 
+	FROM myworld.scaler_student s
+	WHERE s.psp > ANY(
+		SELECT s1.psp
+		FROM myworld.scaler_student s1 
+		WHERE s1.id IN (9,10,7)
+	);
+-- ALL operator (Matches all values in the list.) 
+-- WHERE bid >ALL(1,2,3)   , here, it will try to match all values bid 4,5,6,... will show. 
+	-- give me all students having psp > every psp in the list. 
+	SELECT * 
+	FROM myworld.scaler_student s
+	WHERE s.psp > ALL(
+		SELECT s1.psp
+		FROM myworld.scaler_student s1 
+		WHERE s1.id IN (9,10,7)
+	); 
+-- Exists operator (returns true if count of records >=1 )
+	-- give me all actors who have acted in atleast 1 film. 
+    -- This is a correlated subquery. 
+	SELECT *
+	FROM sakila.actor a
+	WHERE EXISTS(
+		SELECT * 
+		FROM sakila.film_actor fa 
+		WHERE fa.actor_id = a.actor_id 
+	);
 
 ##-------------------------------------------------------------
 #SCENARIO 2: Subquery in FROM clause. 
@@ -101,7 +131,18 @@ WHERE p.MSRP IN
 ##-------------------------------------------------------------
 #SCENARIO 2: Subquery in FROM clause. 
 ##-------------------------------------------------------------
-
+-- 1. give me the costiest product MSRP for each product line. 
+-- 2. get the product name using MSRP  
+-- NOTE : This is not a correlated subquery. 
+SELECT p.*
+FROM 
+(
+	SELECT p.productLine, MAX(p.MSRP) as 'max_msrp'
+	FROM classicmodels.products p 
+	GROUP BY p.productLine
+) AS X
+INNER JOIN classicmodels.products p ON p.productLine = X.productLine AND p.MSRP = X.max_msrp
+;
 ##-------------------------------------------------------------
 #SCENARIO 3: Subquery in SELECT clause. 
 ##-------------------------------------------------------------
